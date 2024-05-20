0fdad586e20541b2c6f4535cbd7cad54459654c8
Tati <tatimb14@gmail.com>
2024-05-20 17:36:46 -0600

    feat: write index and web server

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,44 @@
-use std::{fs};
+use axum::handler::HandlerWithoutStateExt;
+use axum::{http::StatusCode, response::IntoResponse, Router};
+use std::{convert::Infallible, fs, io, net::SocketAddr, path::Path, thread, time::Duration};
+use tower_http::services::ServeDir;
+
 mod templates;
-fn main() {
-    println!("Hello, world!");
-    rebuild_site(content_dir, output_dir)
+const CONTENT_DIR: &str = "content";
+const PUBLIC_DIR: &str = "public";
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    rebuild_site(CONTENT_DIR, PUBLIC_DIR);
+
+    tokio::task::spawn_blocking(move || {
+        println!("listenning for changes: {}", CONTENT_DIR);
+        let mut hotwatch = hotwatch::Hotwatch::new().expect("hotwatch failed to initialize!");
+        hotwatch
+            .watch(CONTENT_DIR, |_| {
+                println!("Rebuilding site");
+                rebuild_site(CONTENT_DIR, PUBLIC_DIR).expect("Rebuilding site");
+            })
+            .expect("failed to watch content folder!");
+        loop {
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    let app = Router::new().nest_service("/", ServeDir::new(PUBLIC_DIR));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!("serving site on {}", addr);
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app.into_make_service())
+        .await
+        .unwrap();
+
+    Ok(())
 }
 
-fn rebuild_site(content_dir: &str, output_dir: &str){
+fn rebuild_site(content_dir: &str, output_dir: &str) -> Result<(), anyhow::Error> {
     let _ = fs::remove_dir_all(output_dir);
-
 
     let markdown_files: Vec<String> = walkdir::WalkDir::new(content_dir)
         .into_iter()
@@ -15,12 +46,54 @@ fn rebuild_site(content_dir: &str, output_dir: &str){
         .filter(|e| e.path().display().to_string().ends_with(".md"))
         .map(|e| e.path().display().to_string())
         .collect();
-    let  html_files: Vec<String>= Vec::with_capacity(markdown_files.len());
+    let mut html_files = Vec::with_capacity(markdown_files.len());
 
-   for file in &markdown_files {
-   println!("test")
-   }
-   
+    for file in &markdown_files {
+        println!("ele");
+        let mut html = templates::HEADER.to_owned();
+        let markdown = fs::read_to_string(&file)?;
+        let parser = pulldown_cmark::Parser::new_ext(&markdown, pulldown_cmark::Options::all());
 
+        let mut body = String::new();
+        pulldown_cmark::html::push_html(&mut body, parser);
 
+        html.push_str(templates::render_body(&body).as_str());
+        html.push_str(templates::FOOTER);
+
+        let html_file = file
+            .replace(content_dir, output_dir)
+            .replace(".md", ".html");
+        let folder = Path::new(&html_file).parent().unwrap();
+        let _ = fs::create_dir_all(folder);
+        fs::write(&html_file, html)?;
+
+        html_files.push(html_file);
+    }
+
+    write_index(html_files, output_dir)?;
+    Ok(())
+}
+
+fn write_index(files: Vec<String>, output_dir: &str) -> Result<(), anyhow::Error> {
+    let mut html = templates::HEADER.to_owned();
+    let body = files
+        .into_iter()
+        .map(|file| {
+            let file = file.trim_start_matches(output_dir);
+            let title = file.trim_start_matches("/").trim_end_matches(".html");
+            format!(r#"<a href="{}">{}</a>"#, file, title)
+        })
+        .collect::<Vec<String>>()
+        .join("<br />\n");
+
+    html.push_str(templates::render_body(&body).as_str());
+    html.push_str(templates::FOOTER);
+
+    let index_path = Path::new(&output_dir).join("index.html");
+    fs::write(index_path, html)?;
+    Ok(())
+}
+
+async fn handle_error(_err: io::Error) -> impl IntoResponse {
+    (StatusCode::INTERNAL_SERVER_ERROR, "Something went wrong...")
 }
